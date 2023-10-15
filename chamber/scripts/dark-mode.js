9043f82bb644e3ca108443cab817659f7249f5bf
TheTubbyRaider <grsraider66@gmail.com>
2023-10-14 22:01:43 -0600

    week 5 chamber dark mode

---
+++ chamber/scripts/dark-mode.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const darkModeSwitch = document.getElementById("dark-mode-switch");
+    const darkModeStylesheet = document.getElementById("dark-mode-stylesheet");
+
+    darkModeSwitch.addEventListener("change", function () {
+        if (darkModeSwitch.checked) {
+            document.body.classList.add("dark-mode");
+            darkModeStylesheet.href = "styles/dark-mode.css";
+        } else {
+            document.body.classList.remove("dark-mode");
+            darkModeStylesheet.href = "";
+        }
+    });
+});
