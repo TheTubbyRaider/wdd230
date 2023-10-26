61f5d30e8e5cb51982618948de989942369ec13c
TheTubbyRaider <grsraider66@gmail.com>
2023-10-26 17:39:11 -0600

    fixing errors

---
+++ lesson07/scripts/scripts.js
@@ -0,0 +1,26 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('#addChapter');
+const list = document.querySelector('.list');
+
+button.addEventListener('click', () => {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+    } else {
+        alert('Please enter a chapter.');
+    }
+    input.focus();
+});
