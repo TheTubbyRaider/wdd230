4c504905279f5d07199c0325c847210727926eba
TheTubbyRaider <grsraider66@gmail.com>
2023-12-16 22:05:43 -0700

    f

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,3 +1,11 @@
+// main.js
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
+
+// Function to fetch and display members
 async function fetchAndDisplayMembers() {
     const response = await fetch('data/members.json');
     const data = await response.json();
@@ -33,17 +41,40 @@ async function fetchAndDisplayMembers() {
 
         membersContainer.appendChild(memberCard);
     });
-
-    // Add or remove the 'list-view' class based on the selected view
-    if (viewSelector === 'list') {
-        membersContainer.classList.add('list-view');
-    } else {
-        membersContainer.classList.remove('list-view');
-    }
 }
 
+// Event listener for view toggle
 document.querySelectorAll('.view-toggle input[name="view"]').forEach(input => {
     input.addEventListener('change', fetchAndDisplayMembers);
 });
 
-fetchAndDisplayMembers();
+// Function to fetch and display weather
+async function fetchAndDisplayWeather() {
+    const apiKey = '9e04f103227aedbab7295408087f118b';
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Orem,us&appid=${9e04f103227aedbab7295408087f118b}`);
+    const data = await response.json();
+
+    document.getElementById('temperature').textContent = data.main.temp;
+    document.getElementById('weather-description').textContent = data.weather[0].description;
+    document.getElementById('windspeed').textContent = data.wind.speed;
+    document.getElementById('windchill').textContent = calculateWindChill(data.main.temp, data.wind.speed);
+}
+
+// Function to calculate wind chill
+function calculateWindChill(temperature, windspeed) {
+    // Your wind chill calculation logic here
+    // Example: return someCalculatedValue;
+}
+
+// Execute the weather fetch on page load
+fetchAndDisplayWeather();
+
+// Event listener for closing the Meet and Greet banner
+document.getElementById('closeBannerBtn').addEventListener('click', closeMeetGreetBanner);
+
+// Function to close the Meet and Greet banner
+function closeMeetGreetBanner() {
+    document.getElementById('meetGreetBanner').style.display = 'none';
+}
+
+// ... (rest of the unchanged code) ...
