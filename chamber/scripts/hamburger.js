ae2c5d62d728e4a9082556a572cbd08cbea56b08
TheTubbyRaider <grsraider66@gmail.com>
2023-12-16 19:41:42 -0700

    f

--- chamber/scripts/hamburger.js
+++ chamber/scripts/hamburger.js
@@ -1,20 +1,20 @@
-// JavaScript for the hamburger menu
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.header-nav .menu ul');
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.querySelector('.hamburger');
+    const menu = document.querySelector('.header-nav .menu nav ul');
 
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('show-menu');
-});
+    hamburger.addEventListener('click', function () {
+        menu.classList.toggle('show-menu');
+    });
 
-// Function to hide the menu on larger screens
-function hideMenuOnLargeScreens() {
-    if (window.innerWidth >= 1024) {
-        menu.classList.remove('show-menu');
+    // Function to hide the menu on larger screens
+    function hideMenuOnLargeScreens() {
+        if (window.innerWidth >= 1024 && menu.classList.contains('show-menu')) {
+            menu.classList.remove('show-menu');
+        }
     }
-}
 
-// Listen for window resize events
-window.addEventListener('resize', hideMenuOnLargeScreens);
-
-// Hide the menu initially on large screens
-hideMenuOnLargeScreens();
+    // Listen for window resize events
+    window.addEventListener('resize', function () {
+        hideMenuOnLargeScreens();
+    });
+});
